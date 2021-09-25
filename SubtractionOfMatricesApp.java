c3c139df871a5a467c8fa6243ae8b1c07e36a8e9
Cyrus-mahle <67721071+Cyrus-mahle@users.noreply.github.com>
2021-09-25 22:25:40 +0200

    Add files via upload

---
+++ SubtractionOfMatricesApp.java
@@ -0,0 +1,11 @@
+package com.gmail.cyrusmahle24;
+
+public class SubtractionOfMatricesApp {
+
+	public static void main(String[] args) {
+		
+		Matrix obj = new Matrix();
+		obj.matrices();
+
+	}
+}
